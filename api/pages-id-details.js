a34e220604268ad29b3cd1e2cf3b8101e6d6800c
ke831 <ncc137k@gmail.com>
2025-07-24 13:33:23 +0900

    쿼리 방식 변경 url 안 -> 밖으로, vercel.json 동적 파일 삭제

--- api/pages-id-details.js
+++ api/pages-id-details.js
@@ -11,7 +11,7 @@ module.exports = async (req, res) => {
   }
 
   // 동적 파라미터(id)는 vercel.json에서 정규식 매핑 시 req.query[0]에 들어감
-  const id = req.query[0];
+  const id = req.query.id;
   console.log('pages-id-details.js: id =', id);
   console.log("req.url:", req.url);
   console.log("req.query:", req.query);
